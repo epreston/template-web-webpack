5c517d58fc31aec980e687e2cea331c84570059d
Ed Preston <epreston@prestonsoft.com>
2021-06-25 21:49:44 +1000

    Make placeholder more noticable.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './styles.css';
+
 console.log('hello world!');
+
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = '<h1>Webpack Web Project</h1>';
+
+  return element;
+}
+
+document.body.appendChild(component());
