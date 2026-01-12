89e2efb081ca95053e33fd8b13b4dd8654f50a8c
Ed Preston <epreston@prestonsoft.com>
2026-01-13 04:50:12 +1100

    Target Modern Browsers
    Switch to yearly baseline system to target modern secure browsers.  Increment the fallback defined in webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const config = {
-  target: ['web', 'es2022'],
+  target: ['web', 'es2024'],
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
